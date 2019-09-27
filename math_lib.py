15cae3635f520333f988c2bdc305820f165b8b66
Fletcher Collins <fletcher@colli.nz>
2019-09-26 22:06:02 -0600

    add note on element removal to math_lib

--- math_lib.py
+++ math_lib.py
@@ -5,6 +5,9 @@ def list_mean(L):
         return None
 
     for l in L:
+        # NOTE: here we drop any non number from the list before computing the
+        # mean. Because the list is consistent between the scope inside this
+        # function and outside, modifying the list in this fashion stays.
         if not isinstance(l, (int, float)):
             print("Non number " + l + " in list, dropping.")
             L.remove(l)
@@ -22,6 +25,8 @@ def list_stdev(L):
 
     stdev = 0
 
+    # calling list_mean has the side effect of also removing any non numbers
+    # from the list we're working with
     mean = list_mean(L)
     for l in L:
         stdev += (l - mean) ** 2
