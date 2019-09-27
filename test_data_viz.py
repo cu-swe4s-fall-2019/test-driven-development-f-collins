eb99cc4d63e7c220b79ef923dd62aeaa20486485
Fletcher Collins <fletcher@colli.nz>
2019-09-27 00:00:53 -0600

    add tests to see if data_viz methods created a file

--- test_data_viz.py
+++ test_data_viz.py
@@ -1,15 +1,38 @@
 import unittest
+import os
 import data_viz as dv
 
 class TestDataViz(unittest.TestCase):
     def test_boxplot_file_name_none(self):
         self.assertIsNone(dv.boxplot(None, None), None)
+
+    def test_boxplot_file_created(self):
+        outfile = "output.png"
+        dv.boxplot([1], outfile)
+        self.assertTrue(os.path.exists(outfile))
+        os.remove(outfile)
+
+
+
+
         
     def test_histogram_file_name_none(self):
         self.assertIsNone(dv.histogram(None, None), None)
 
+    def test_histogram_file_created(self):
+        outfile = "output.png"
+        dv.histogram([1], outfile)
+        self.assertTrue(os.path.exists(outfile))
+        os.remove(outfile)
+
     def test_combo_file_name_none(self):
         self.assertIsNone(dv.combo(None, None), None)
+
+    def test_combo_file_created(self):
+        outfile = "output.png"
+        dv.combo([1], outfile)
+        self.assertTrue(os.path.exists(outfile))
+        os.remove(outfile)
 
 if __name__ == "__main__":
     unittest.main()
