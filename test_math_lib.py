c853e6ced78ca0d49d0ff08b392deb2bce38bfd7
Fletcher Collins <fletcher@colli.nz>
2019-09-26 20:31:06 -0600

    add list_mean empty list input test

--- test_math_lib.py
+++ test_math_lib.py
@@ -5,6 +5,9 @@ class TestMathlib(unittest.TestCase):
     def test_list_mean_none(self):
         self.assertIsNone(ml.list_mean(None), None)
 
+    def test_list_mean_empty_list(self):
+        self.assertIsNone(ml.list_mean([]), None)
+
 if __name__ == "__main__":
     unittest.main()
 
