442cd8dd3ef9040d0bf8eb037ec9d370bacf9908
Fletcher Collins <fletcher@colli.nz>
2019-09-26 23:41:26 -0600

    handle null filenames in data_viz

--- data_viz.py
+++ data_viz.py
@@ -1,9 +1,15 @@
 
 def boxplot(L, out_file_name):
+    if out_file_name == None:
+        return None
     pass
 
 def histogram(L, out_file_name):
+    if out_file_name == None:
+        return None
     pass
 
 def combo(L, out_file_name):
+    if out_file_name == None:
+        return None
     pass
