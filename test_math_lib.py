f4b4ed59b04c5cf8a063ffbf814522e63445c4d0
Fletcher Collins <fletcher@colli.nz>
2019-09-26 20:49:43 -0600

    add list of random floats test to test_math_lib

--- test_math_lib.py
+++ test_math_lib.py
@@ -13,13 +13,19 @@ class TestMathlib(unittest.TestCase):
     def test_list_mean_one_list(self):
         self.assertEqual(ml.list_mean([1, 1, 1, 1, 1]), 1)
 
-    def test_list_mean_random_list(self):
+    def test_list_mean_random_int_list(self):
         for i in range(100):
             L = []
             for j in range(10):
                 L.append(random.randint(0,100))
             self.assertEqual(ml.list_mean(L), statistics.mean(L))
 
+    def test_list_mean_random_float_list(self):
+        for i in range(100):
+            L = []
+            for j in range(10):
+                L.append(random.uniform(0,100))
+            self.assertAlmostEqual(ml.list_mean(L), statistics.mean(L))
 
 if __name__ == "__main__":
     unittest.main()
