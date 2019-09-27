f67adad6d040d662adaaaea9b743a6a32d40267d
Fletcher Collins <fletcher@colli.nz>
2019-09-26 20:37:33 -0600

    add list of ones test in test_math_lib

--- test_math_lib.py
+++ test_math_lib.py
@@ -8,6 +8,9 @@ class TestMathlib(unittest.TestCase):
     def test_list_mean_empty_list(self):
         self.assertIsNone(ml.list_mean([]), None)
 
+    def test_list_mean_one_list(self):
+        self.assertEqual(ml.list_mean([1, 1, 1, 1, 1]), 1)
+
 if __name__ == "__main__":
     unittest.main()
 
