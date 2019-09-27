319ccd2086c47dcf92fc4d01c685a848a5819318
Fletcher Collins <fletcher@colli.nz>
2019-09-26 21:21:51 -0600

    update list_mean to drop non numbers in list from calculation

--- math_lib.py
+++ math_lib.py
@@ -2,8 +2,15 @@
 def list_mean(L):
     if L == None:
         return None
+
+    for l in L:
+        if not isinstance(l, (int, float)):
+            print("None number " + l + " in list, dropping.")
+            L.remove(l)
+
     if len(L) == 0:
         return None
+
     return sum(L)/len(L)
 
 def list_stdev(L):
